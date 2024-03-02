aef0ab0c6a6404413ccccad3e93dfb6c678b8139
Ridwaan hassan <160154843+aragt@users.noreply.github.com>
2024-03-01 23:17:20 -0800

    maamul

---
+++ script.js
@@ -0,0 +1,18 @@
+let typed = new typed('lan',(
+    Strings: [ 'front-end developer!','freelancer!','UI designer!','youtuber!' ],
+    TypeSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+    backSpeed:100,
+))
+let typed = new Typed('.lan',(
+
+strings: ['Front-End Developer!', 'Freelancer!', 'UI Designer!', 'YouTuber!'],
+
+typedpeed: 100,
+
+backspeed: 100,
+
+backDelay: 12000,
+
+loop))
